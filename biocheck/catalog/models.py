3b5a7ac6339e00ace1ea0c174095dbdd4c590d01
Veronica Surkova <srkveronica@gmail.com>
2024-05-14 20:02:53 +0300

    Create and translate admin-zone

--- biocheck/catalog/models.py
+++ biocheck/catalog/models.py
@@ -2,8 +2,15 @@ from django.db import models
 
 
 class FoodAdditives(models.Model):
-    e_title = models.IntegerField()
-    common_title = models.CharField(max_length=125)
-    category = models.CharField(max_length=125)
-    description = models.TextField()
-    is_dangerous = models.BooleanField()
+    e_title = models.IntegerField(verbose_name='номер E')
+    common_title = models.CharField(max_length=125, verbose_name='название')
+    category = models.CharField(max_length=125, verbose_name='категория')
+    description = models.TextField(verbose_name='описание')
+    is_dangerous = models.BooleanField(verbose_name='опасна')
+
+    class Meta:
+        verbose_name = 'пищевая добавка'
+        verbose_name_plural = 'Пищевые добавки'
+
+    def __str__(self):
+        return self.e_title
