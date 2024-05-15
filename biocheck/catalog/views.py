fc592d57e25a6117c3b57f946a269f124646e38a
Veronica Surkova <srkveronica@gmail.com>
2024-05-16 01:00:08 +0300

    Implement search

--- biocheck/catalog/views.py
+++ biocheck/catalog/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 from .models import FoodAdditives
 
 
 def fa_list(request):
-    template_name = 'catalog/fa_list.html'
+    template_name = 'catalog/fa_list.html'             
     fa_catalog = FoodAdditives.objects.values( 'id',
         'e_title', 'common_title', 'category'
         ).order_by('e_title')
+    query = request.GET.get('search')
+    if query:
+        fa_catalog = fa_catalog.filter(
+            Q(e_title__icontains=query) | 
+            Q(common_title__icontains=query)
+        )
     context = {
         'fa_catalog': fa_catalog
     }
     return render(request, template_name, context)
+
 
 
 def fa_detail(request, pk):
