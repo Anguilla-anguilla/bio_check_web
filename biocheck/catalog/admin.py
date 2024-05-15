face3f73b58e0e8b888cd424d3231b43dbd5fdbe
Veronica Surkova <srkveronica@gmail.com>
2024-05-15 19:58:14 +0300

    Add is_not_informative field

--- biocheck/catalog/admin.py
+++ biocheck/catalog/admin.py
@@ -1,5 +1,29 @@
 from django.contrib import admin
 from .models import FoodAdditives
 
-admin.site.register(FoodAdditives)
 
+class FoodAdditivesAdmin(admin.ModelAdmin):
+    list_display = (
+        'e_title',
+        'common_title',
+        'category',
+        'description',
+        'is_dangerous',
+        'is_not_informative'
+    )
+    list_editable = (
+        'common_title',
+        'category',
+        'description',
+        'is_dangerous',
+        'is_not_informative'
+    )
+    search_fields = (
+        'e_title',
+        'common_title'
+    )
+    list_filter = ('category', 'is_not_informative')
+    list_display_links = ('e_title',)
+
+
+admin.site.register(FoodAdditives, FoodAdditivesAdmin)
--- biocheck/catalog/models.py
+++ biocheck/catalog/models.py
@@ -5,8 +5,12 @@ class FoodAdditives(models.Model):
     e_title = models.IntegerField(verbose_name='номер E')
     common_title = models.CharField(max_length=125, verbose_name='название')
     category = models.CharField(max_length=125, verbose_name='категория')
-    description = models.TextField(verbose_name='описание')
+    description = models.TextField(verbose_name='описание', blank=True)
     is_dangerous = models.BooleanField(verbose_name='опасна')
+    is_not_informative = models.BooleanField(
+        verbose_name='нет информации по добавке',
+        default=False
+        )
 
     class Meta:
         verbose_name = 'пищевая добавка'
